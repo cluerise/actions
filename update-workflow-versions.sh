c5a70d8fcff603abd2c8c19f4eb47f682d268851
Branislav Holý <brano@holy.am>
2023-09-16 13:10:32 +0200

    chore: Use version in workflow calls only

--- update-workflow-versions.sh
+++ update-workflow-versions.sh
@@ -1,17 +1,16 @@
-sedReplaceOptions="-i"
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  sedReplaceOptions="-i ''"
-fi
-
 versionRegexp="[a-z0-9\.-]+"
-regexp="(cluenar\/actions\/.+@)($versionRegexp)([ \n]*)"
+regexp="(CLUENAR_ACTIONS_VERSION: v)($versionRegexp)([ \n]*)"
 version=$(sed -n -E "s/\"version\": \"($versionRegexp)\",/\1/p" package.json | xargs)
 
 find . -type d -name "node_modules" -prune -o -type f -name "*.yaml" -o -name "*.yml" |
 while read -r file; do
   if [ ! -d "$file" ]; then
     if grep -q -E "$regexp" "$file"; then
-      sed $sedReplaceOptions -E "s/$regexp/\1v$version\3/g" "$file"
+      if [[ "$OSTYPE" == "darwin"* ]]; then
+        sed -i '' -E "s/$regexp/\1$version\3/g" "$file"
+      else
+        sed -i -E "s/$regexp/\1$version\3/g" "$file"
+      fi
     fi
   fi
 done
