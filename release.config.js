e9c97a37f68ab3ce4d340e672a844305407bd778
Branislav Holý <brano@holy.am>
2023-11-26 13:29:20 +0100

    chore: Release updates of actions (#89)

--- release.config.js
+++ release.config.js
@@ -1,7 +1,16 @@
-import { createReleaseConfig } from '@cluenar/tools/dist/configs/release.config.js';
+import {
+  createReleaseConfig,
+  defaultChangelogTypes,
+  defaultReleaseRules
+} from '@cluenar/tools/dist/configs/release.config.js';
+
+const releaseRules = [...defaultReleaseRules, { type: 'deps', scope: 'actions', release: 'patch' }];
+const changelogTypes = [...defaultChangelogTypes, { type: 'deps', scope: 'actions', section: 'Dependency updates' }];
 
 export default createReleaseConfig({
   host: 'https://github.com',
   owner: 'cluenar',
-  repository: 'actions'
+  repository: 'actions',
+  releaseRules,
+  changelogTypes
 });
