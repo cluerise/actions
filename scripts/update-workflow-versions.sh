eef8b5de71f58bd2e8b61beac131851e7afac751
Branislav Holý <brano@holy.am>
2025-06-25 11:11:34 +0200

    deps(dev): Reinit with @cluerise/tools 4.2.0

--- scripts/update-workflow-versions.sh
+++ scripts/update-workflow-versions.sh
@@ -4,15 +4,15 @@ versionRegexp="[a-z0-9\.-]+"
 regexp="(cluerise\/actions\/.+@v)($versionRegexp)([ \n]*)"
 version=$(sed -n -E "s/\"version\": \"($versionRegexp)\",/\1/p" package.json | xargs)
 
-find . -type d -name "node_modules" -prune -o -type f -name "*.yaml" -o -name "*.yml" |
-while read -r file; do
-  if [ ! -d "$file" ]; then
-    if grep -q -E "$regexp" "$file"; then
-      if [[ "$OSTYPE" == "darwin"* ]]; then
-        sed -i '' -E "s/$regexp/\1$version\3/g" "$file"
-      else
-        sed -i -E "s/$regexp/\1$version\3/g" "$file"
+find . -type d -name "node_modules" -prune -o -type f -name "*.yaml" -o -name "*.yml" \
+  | while read -r file; do
+    if [ ! -d "$file" ]; then
+      if grep -q -E "$regexp" "$file"; then
+        if [[ "$OSTYPE" == "darwin"* ]]; then
+          sed -i '' -E "s/$regexp/\1$version\3/g" "$file"
+        else
+          sed -i -E "s/$regexp/\1$version\3/g" "$file"
+        fi
       fi
     fi
-  fi
-done
+  done
